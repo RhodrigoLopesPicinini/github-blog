ad445f4575f63abdb7c8932c4ba18c31c7199191
Rhodrigo Picinini <rhodrigolpicinini@hotmail.com>
2023-08-17 11:01:03 -0300

    feat: :tada: project start, eslint and styles configured

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# github-blog
-Terceiro desafio da trilha de ReactJS do curso Ignite da Rocketseat.
+# Github Blog
+
+Github Blog é uma SPA utilizando React e Typescript, que listará issues criadas deste repositório para simular uma página de um blog real, utilizando a API do Github.
+
+Terceiro desafio da trilha de ReactJS do curso Ignite da Rocketseat. Este projeto servirá para treinar conhecimentos básicos de requisições HTTP e suas diversas interações com um servidor fake como backend.
---
+++ src/styles/global.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        background-color: ${(props) => props.theme['base-background']};
+        color: ${(props) => props.theme['base-text']};
+    }
+
+    body, input, textarea, button {
+        font: 400 1rem Nunito, sans-serif;
+    }
+
+    h1, h2, h3 {
+        color: ${(props) => props.theme['base-title']};
+    }
+
+    :focus {
+        outline: 0;
+        box-shadow: 0 0 0 2px ${(props) => props.theme['brand-blue']};
+    }
+`
